da17e089ce67e01578a1e5c9871a29b6c189fc54
Jason Rudolph <jason@jasonrudolph.com>
2021-06-15 14:31:10 -0400

    Add multiple tests to demonstrate parallel execution

--- spec/example_0_spec.rb
+++ spec/example_0_spec.rb
@@ -1,4 +1,4 @@
-describe 'Example' do
+describe 'Example 0' do
   it 'passes' do
     expect(true).to eq true
   end
---
+++ spec/example_1_spec.rb
@@ -0,0 +1,5 @@
+describe 'Example 1' do
+  it 'passes' do
+    expect(true).to eq true
+  end
+end
